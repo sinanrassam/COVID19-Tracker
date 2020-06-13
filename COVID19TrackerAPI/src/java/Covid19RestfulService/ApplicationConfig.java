19603d9bdcbc2904b47e88e6b11ef4c1b82e49b4
Sinan Rassam <sinan.rassam@outlook.com>
2020-06-13 22:53:01 +1200

    Revert "Reverted from storing tag ids due to RID (dynamic tag ids)"
    This reverts commit e766ab1b94ad57184bb505f1737af2042902d2c8.
    
    Reverting revert

--- COVID19TrackerAPI/src/java/Covid19RestfulService/ApplicationConfig.java
+++ COVID19TrackerAPI/src/java/Covid19RestfulService/ApplicationConfig.java
@@ -23,6 +23,7 @@ public class ApplicationConfig extends Application {
      * out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(Covid19RestfulService.TagsResource.class);
         resources.add(Covid19RestfulService.TracksResource.class);
         resources.add(Covid19RestfulService.UsersResource.class);
     }
