3c1ff5fb8fe2f72460c47319ecbe9fbf3d822ccb
Sinan Rassam <sinan.rassam@outlook.com>
2020-06-13 23:46:21 +1200

    Improved settings and register activities

--- COVID19TrackerApp/app/src/main/java/com/sinanrassam/covid19tracker/SettingsActivity.java
+++ COVID19TrackerApp/app/src/main/java/com/sinanrassam/covid19tracker/SettingsActivity.java
@@ -16,6 +16,7 @@ import com.sinanrassam.covid19tracker.Utils.PreferencesUtility;
 public class SettingsActivity extends AppCompatActivity {
 
     TextView mApiUrl;
+    boolean isLoggedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +32,28 @@ public class SettingsActivity extends AppCompatActivity {
         TextView mEmail = findViewById(R.id.emailProfile);
 
         User user = PreferencesUtility.getUser();
-        if ((user.getUsername() != null) && (user.getUsername() != "")) {
+        isLoggedIn = ((user.getUsername() != null) && (user.getUsername() != ""));
+        if (isLoggedIn) {
             mName.setText("Hi, " + user.getFirstName() + " " + user.getLastName() + "!");
             mUsername.setText("Your username is: " + user.getUsername());
             mEmail.setText("Your email address is: " + user.getEmail());
+        }
 
             findViewById(R.id.btn_logout).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    PreferencesUtility.setUserInfo(new User());
-                    Toast.makeText(getApplicationContext(), R.string.action_logout_successful, Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(view.getContext(), LoginActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                    finish();
+                    if (isLoggedIn) {
+                        PreferencesUtility.setUserInfo(new User());
+                        Toast.makeText(getApplicationContext(), R.string.action_logout_successful, Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(view.getContext(), LoginActivity.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(), R.string.action_already_logged_out, Toast.LENGTH_SHORT).show();
+                    }
                 }
             });
-        }
 
         mApiUrl = findViewById(R.id.apiUrl);
         mApiUrl.setText(PreferencesUtility.getApiUrl());
