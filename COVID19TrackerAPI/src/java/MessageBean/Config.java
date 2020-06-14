c987e750f4c6ee66bc8aa66e205c6c30f9e8726f
Sinan Rassam <sinan.rassam@outlook.com>
2020-06-14 13:46:23 +1200

    Was able to utilize server enterprise messaging to send commands and update the database

---
+++ COVID19TrackerAPI/src/java/MessageBean/Config.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MessageBean;
+
+/**
+ *
+ * @author sinan
+ */
+public class Config {
+    public final static String MESSAGE_REASON = "Update Location -> ";
+    
+}
